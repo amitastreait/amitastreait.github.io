c5b56cad7fed3f93935f7652921acfb81149c451
Amit Singh <cse.amitallenhouse@gmail.com>
2021-04-13 20:32:15 +0530

    Initial commit

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["YouTuber", "Architect", "Blogger", "Instructor","Developer", "Freelancer"],
+        strings: ["YouTuber", "Architect", "Blogger", "Instructor", "Developer", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
