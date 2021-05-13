39de541b62044d36de4a8bf1a233f63f68b3ca6d
Amit Singh <cse.amitallenhouse@gmail.com>
2021-05-13 22:01:27 +0530

    Modified Titiles

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["YouTuber", "Architect", "Blogger", "Instructor", "Developer", "Freelancer"],
+        strings: ["YouTuber", "An Architect", "Blogger", "An Instructor", "Developer", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
